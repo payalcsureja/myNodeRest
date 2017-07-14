893f9a1c5dea65d4600e08f2c5f015fab0edc0bb
payal <payalcsureja@gmail.com>
2017-07-14 15:34:27 -0400

    mogran log setup and basic changes

--- app/routes/test_routes.js
+++ app/routes/test_routes.js
@@ -1,10 +1,15 @@
 //initialize
 module.exports = function (routes) {
+    routes.get('/test', test);
     routes.get('/tests', tests);
     // routes.get('/users', users);
 }
 
 //APIs
+function test(req, res) {    
+    res.send("Get all test.");
+}
+
 function tests(req, res) {    
     res.send("Get all tests.");
 }
--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express        = require('express'); // call express
 const app            = express(); // define our app using express
 const bodyParser     = require('body-parser'); // parses the body portion of an incoming HTTP request and makes it easier to extract different parts of the contained information
+const morgan = require('morgan'); // HTTP request logger middleware for node.js
+// const methodOverride = require('method-override');
 
 // ROUTES FOR OUR API
 const routes = require('./app/routes')(express);
@@ -11,8 +13,11 @@ const routes = require('./app/routes')(express);
 const port = process.env.PORT || 8080;  // run PORT=4444 node index.js, Node will use process.env.PORT which equals to 4444 // process.env.PORT from Heroku/...
 
 // configure app to use bodyParser() ,this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));  // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+// app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+// app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(morgan('dev')); // log every request to the console
 
 // REGISTER OUR ROUTES - Connect all our routes to our application
 // all of our routes will be prefixed with /api
