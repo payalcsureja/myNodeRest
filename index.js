f742f2633481ebe7f4e875261f9f21e11b14b484
payal <payalcsureja@gmail.com>
2017-08-03 14:09:47 -0400

    override with the X-HTTP-Method-Override header in the request

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app            = express(); // define our app using express
 const bodyParser     = require('body-parser'); // parses the body portion of an incoming HTTP request and makes it easier to extract different parts of the contained information
 const morgan = require('morgan'); // HTTP request logger middleware for node.js
 const helmet = require('helmet'); // Helmet helps you secure your Express apps by setting various HTTP headers.
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override'); // Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 
 const config = require('./app/config/app.config'); // looks for app.config.js
 
@@ -34,12 +34,12 @@ app.use(bodyParser.urlencoded({ extended: true }));  // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 // app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 
-// app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 // override with different headers; last one takes precedence
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 // app.use(methodOverride('X-HTTP-Method'))          // Microsoft
 // app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
 // app.use(methodOverride('X-Method-Override'))      // IBM
-
+// app.use(methodOverride('_method')) // override with POST having ?_method=DELETE
 // NOTE: when using req.body, you must fully parse the request body via app.use(bodyParser.urlencoded())
 //       before you call methodOverride() in your middleware stack,
 //       otherwise req.body will not be populated.
@@ -51,6 +51,7 @@ app.use(bodyParser.json()); // parse application/json
 //     return method
 //   }
 // }));
+
 if(config.debug) {
   app.use(morgan(config.debugMorganType)); // log every request to the console
 }
